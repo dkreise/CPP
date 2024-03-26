db6dd7c34645ba1049235a94a7a56b67ae5ce6e7
Diana Kreise <dkreise@car2s6.42barcelona.com>
2024-03-26 12:45:34 +0100

    ex01 done

--- cpp_01/ex01/Zombie.hpp
+++ cpp_01/ex01/Zombie.hpp
@@ -9,9 +9,11 @@ class Zombie
         Zombie(void);
         Zombie(std::string name);
         ~Zombie(void);
+
         void announce(void);
         void set_name(std::string name);
         std::string get_name(void);
+        
     private:
         std::string name;
 };
--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -10,9 +10,21 @@ int main(void)
 
     std::cout << "First zombie: " << (*zombies).get_name() << std::endl << std::endl;
 
+    std::cout << "Names:" << std::endl;
     for (int i = 0; i < N; i ++)
         std::cout << i + 1 << ": " << zombies[i].get_name() << std::endl;
 
+    std::cout << std::endl << "Announce function:" << std::endl;
+    for (int i = 0; i < N; i ++)
+        zombies[i].announce();
+
+    std::cout << std::endl << "Announce function after changing first name:" << std::endl;
+    zombies[0].set_name("first");
+    for (int i = 0; i < N; i ++)
+        zombies[i].announce();
+
+    std::cout << std::endl;
     delete [] zombies;
+
     return (0);
 }
