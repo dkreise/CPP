2d37417fe17c9752a08534df57ca69d8a1aaeadb
dkreise <dia.kreis@gmail.com>
2024-05-09 12:18:24 +0200

    organising tests and recivered cat files

--- cpp_04/ex01/Cat.cpp
+++ cpp_04/ex01/Cat.cpp
@@ -2,23 +2,28 @@
 
 Cat::Cat(void) : Animal("Cat")
 {
-    this->brain = new Brain();
+    this->_brain = new Brain();
     std::cout << "Cat constructor called" << std::endl;
 }
 
-Cat::Cat(Cat const & src)
+Cat::Cat(Cat const & src) : Animal(src)
 {
-    *this = src;
+    this->_brain = new Brain(*src._brain);
     std::cout << "Cat copy constructor called" << std::endl;
 }
 
 Cat::~Cat(void)
 {
+    delete (this->_brain);
     std::cout << "Cat destructor called" << std::endl;
 }
 
 Cat & Cat::operator=(Cat const & r)
 {
+    std::cout << "Cat copy assignment operator called" << std::endl;
+    // if (this->brain) ??????????????
+    delete (this->_brain);
+    this->_brain = new Brain(*r._brain);
     this->type = r.type;
     return (*this);
 }
@@ -26,4 +31,14 @@ Cat & Cat::operator=(Cat const & r)
 void Cat::makeSound(void) const
 {
     std::cout << "Meow" << std::endl;
+}
+
+void Cat::printIdeas(void) const
+{
+    this->_brain->printIdeas();
+}
+
+void Cat::addIdea(std::string idea)
+{
+    this->_brain->addIdea(idea);
 }
--- cpp_04/ex01/main.cpp
+++ cpp_04/ex01/main.cpp
@@ -4,69 +4,81 @@
 #include "Brain.hpp"
 
 int main(void)
-{/*
+{
+    std::cout << "--------------------------------------" << std::endl;
+    std::cout << "--- CREATING A CAT: ---" << std::endl;
     Cat cat = Cat();
     cat.makeSound();
     cat.printIdeas();
     cat.addIdea("im a cat");
+    std::cout << "--- ADDED 1 IDEA: ---" << std::endl;
     cat.printIdeas();
     for (int i = 0; i < 110; i ++)
         cat.addIdea("a");
+    std::cout << "--- ADDED 110 IDEAS: ---" << std::endl;
     cat.printIdeas();
     for (int i = 0; i < 10; i ++)
         cat.addIdea("b");
-    cat.printIdeas();*/
+    std::cout << "--- ADDED 10 IDEAS: ---" << std::endl;
+    cat.printIdeas();
     std::cout << "--------------------------------------" << std::endl;
+    std::cout << "--- CHECKING IF THE EMPTY BRAIN CAN BE DELETED: ---" << std::endl;
+    // delete this section later
     Dog* d = new Dog();
     delete d;
     std::cout << "--------------------------------------" << std::endl;
-    Dog* d2 = new Dog();
-    d2->addIdea("lalala");
-    Dog* d3 = new Dog();
-    *d2 = *d3;
+    std::cout << "--- CHECKING IF THE BRAIN IS DELETED BEFORE REASSIGNMENT AND NO LEAKS: ---" << std::endl;
+    // MAYBE ALSO DELETE LATER
+    Dog* dd2 = new Dog();
+    dd2->addIdea("lalala");
+    Dog* dd3 = new Dog();
+    *dd2 = *dd3;
+    delete dd2;
+    delete dd3;
 
     std::cout << "--------------------------------------" << std::endl;
-    // check copies
+    std::cout << "--- CHECKING THAT COPIES ARE DEEP: ---" << std::endl;
     Dog dog = Dog();
     dog.addIdea("im a dog");
-    std::cout << "---ideas of dog:" << std::endl;
+    std::cout << "--- ideas of dog:" << std::endl;
     dog.printIdeas();
     Dog dogcopy = Dog(dog);
-    std::cout << "---ideas of copy dog:" << std::endl;
+    std::cout << "--- ideas of copy dog:" << std::endl;
     dogcopy.printIdeas();
     dogcopy.addIdea("im a copy");
-    std::cout << "---ideas of copy dog:" << std::endl;
+    std::cout << "--- ideas of copy dog:" << std::endl;
     dogcopy.printIdeas();
-    std::cout << "---ideas of dog:" << std::endl;
+    std::cout << "--- ideas of dog:" << std::endl;
     dog.printIdeas();
 
     std::cout << "--------------------------------------" << std::endl;
-    // check copies with new
+    std::cout << "--- CHECKING THAT POINTER COPIES ARE DEEP: ---" << std::endl;
     Dog* dogp = new Dog();
     dogp->addIdea("im a dog");
-    std::cout << "---ideas of dog:" << std::endl;
+    std::cout << "--- ideas of dog:" << std::endl;
     dogp->printIdeas();
     Dog* dogcopyp = new Dog(*dogp);
-    std::cout << "---ideas of copy dog:" << std::endl;
+    std::cout << "--- ideas of copy dog:" << std::endl;
     dogcopyp->printIdeas();
     dogcopyp->addIdea("im a copy");
-    std::cout << "---ideas of copy dog:" << std::endl;
+    std::cout << "--- ideas of copy dog:" << std::endl;
     dogcopyp->printIdeas();
-    std::cout << "---ideas of dog:" << std::endl;
+    std::cout << "--- ideas of dog:" << std::endl;
     dogp->printIdeas();
 
     delete dogp;
     delete dogcopyp;
-/*
+
     std::cout << "--------------------------------------" << std::endl;
+    std::cout << "--- SUBJECT EXPAMLE THAT SHOULD NOT CREATE A LEAK: ---" << std::endl;
     const Animal* acat = new Cat();
 
-    delete acat; // shouldn't be leaks
+    delete acat;
 
     std::cout << "--------------------------------------" << std::endl;
 
     // Animal as[] = {Cat(), Dog()}; --- working
-
+    std::cout << "--- CREATING ARRAY OF DIFFERENT ANIMALS: ---" << std::endl;
     Cat *c1 = new Cat();
     Cat *c2 = new Cat();
     Animal *c3 = new Cat();
@@ -75,6 +87,7 @@ int main(void)
     Animal *d3 = new Dog();
 
     Animal *a[] = {c1, c2, c3, d1, d2, d3};
+    std::cout << "--- SOUNDS OF EACH ANIMAL IN THE ARRAY AND DELETING: ---" << std::endl;
     for (int i = 0; i < 6; i ++)
     {
         a[i]->makeSound();
@@ -90,6 +103,6 @@ int main(void)
     // Animal cat1 = Cat();
     // //Dog dog1(), dog2(), dog3();
     // Animal *animals2[] = {&cat1};
-*/
+
     return (0);
 }
