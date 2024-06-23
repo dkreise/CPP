c317b4160a920437fb06fa3fea6ae29556d483af
Diana Kreise <dia.kreis@gmail.com>
2024-06-23 13:29:12 +0200

    added more tests to main

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -13,5 +13,30 @@ int main(void)
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
 
+    Span sp2 = Span(7);
+    try
+    {
+        sp2.addRange(1, 10);
+        std::cout << sp2.shortestSpan() << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    sp2.addNumber(100);
+    sp2.addRange(1, 5);
+    std::cout << sp2.shortestSpan() << std::endl;
+    std::cout << sp2.longestSpan() << std::endl;
+
+    Span sp3 = Span(10);
+    sp3.addRange(2, 7);
+    Span sp3_copy = Span(sp3);
+    sp3_copy.addNumber(42);
+    sp3 = sp2;
+    std::cout << sp3.shortestSpan() << std::endl;
+    std::cout << sp3.longestSpan() << std::endl;
+    std::cout << sp3_copy.shortestSpan() << std::endl;
+    std::cout << sp3_copy.longestSpan() << std::endl;
+
     return (0);
 }
