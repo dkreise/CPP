f87893f997290847854d81a13aad645f329d2e30
Diana Kreise <dia.kreis@gmail.com>
2024-06-23 13:28:33 +0200

    started ex02

---
+++ cpp_08/ex02/MutantStack.hpp
@@ -0,0 +1,14 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+
+};
+
+#endif
