d0538e2bb8302c79e1a254f1757439cd5fc4eb7d
dkreise <dia.kreis@gmail.com>
2024-06-24 14:26:28 +0200

    ex02 done

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -8,7 +8,26 @@
 template<typename T>
 class MutantStack : public std::stack<T>
 {
+    public:
+        MutantStack(void) : std::stack<T>() {}
+        MutantStack(MutantStack const & src) : std::stack<T>(src) {}
+        ~MutantStack(void) {}
 
+        MutantStack & operator=(MutantStack const & src)
+        {
+            return (std::stack<T>::operatot=(src));
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        iterator begin(void)
+        {
+            return (this->c.begin());
+        }
+        iterator end(void)
+        {
+            return (this->c.end());
+        }
 };
 
 #endif
