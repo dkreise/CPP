ebef2ea905513d28a6f9bb6ecd707fa7bc3e8d4d
Diana Kreise <dia.kreis@gmail.com>
2024-07-18 17:07:29 +0200

    cleaned main

--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -9,18 +9,19 @@ int main(void)
     v[4] = 42;
     v[5] = 17;
 
+    std::cout << std::endl << "For array:" << std::endl;
     try
     {
         std::vector<int>::iterator it = easyfind(v, 3);
-        std::cout << std::distance(v.begin(), it) << std::endl;
+        std::cout << "index of val 3: " << std::distance(v.begin(), it) << std::endl;
         it = easyfind(v, 42);
-        std::cout << std::distance(v.begin(), it) << std::endl;
+        std::cout << "index of val 42: " << std::distance(v.begin(), it) << std::endl;
         it = easyfind(v, 33);
-        std::cout << std::distance(v.begin(), it) << std::endl;
+        std::cout << "index of val 33: " << std::distance(v.begin(), it) << std::endl << std::endl;
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << std::endl;
+        std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
     }
     
     std::list<int> lst;
@@ -29,17 +30,18 @@ int main(void)
     lst.push_back(7);
     lst.push_back(3);
 
+    std::cout << "For list:" << std::endl;
     try
     {
         std::list<int>::iterator it = easyfind(lst, 7);
-        std::cout << std::distance(lst.begin(), it) << std::endl;
+        std::cout << "index of val 7: " << std::distance(lst.begin(), it) << std::endl;
         it = easyfind(lst, 420);
-        std::cout << std::distance(lst.begin(), it) << std::endl;
+        std::cout << "index of val 420: " << std::distance(lst.begin(), it) << std::endl;
         it = easyfind(lst, 42);
-        std::cout << std::distance(lst.begin(), it) << std::endl;
+        std::cout << "index of val 42: " << std::distance(lst.begin(), it) << std::endl;
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << std::endl;
+        std::cerr << "ERROR: " << e.what() << std::endl;
     }
 }
