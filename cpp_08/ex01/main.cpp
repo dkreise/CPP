f6a785ec57151d176bae0b8c35e55af7b98b316b
jareste <dia.kreis@gmail.com>
2024-07-21 16:04:18 +0200

    cleaning main

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -10,8 +10,8 @@ int main(void)
     sp.addNumber(9);
     sp.addNumber(11);
 
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    std::cout <<  "Shortest span for sp: " << sp.shortestSpan() << std::endl;
+    std::cout << "Longest span for sp: " << sp.longestSpan() << std::endl;
 
     Span sp2 = Span(7);
     try
@@ -21,22 +21,22 @@ int main(void)
     }
     catch (const std::exception& e)
     {
-        std::cerr << e.what() << std::endl;
+        std::cerr << "ERROR: " << e.what() << std::endl;
     }
     sp2.addNumber(100);
-    sp2.addRange(1, 5);
-    std::cout << sp2.shortestSpan() << std::endl;
-    std::cout << sp2.longestSpan() << std::endl;
+    sp2.addRange(-2, 2);
+    std::cout << "Shortest span for sp2: " << sp2.shortestSpan() << std::endl;
+    std::cout << "Longest span for sp2: " << sp2.longestSpan() << std::endl;
 
     Span sp3 = Span(10);
     sp3.addRange(2, 7);
     Span sp3_copy = Span(sp3);
     sp3_copy.addNumber(42);
     sp3 = sp2;
-    std::cout << sp3.shortestSpan() << std::endl;
-    std::cout << sp3.longestSpan() << std::endl;
-    std::cout << sp3_copy.shortestSpan() << std::endl;
-    std::cout << sp3_copy.longestSpan() << std::endl;
+    std::cout << "Shortest span for sp3: " << sp3.shortestSpan() << std::endl;
+    std::cout << "Longest span for sp3: " << sp3.longestSpan() << std::endl;
+    std::cout << "Shortest span for sp3copy: " << sp3_copy.shortestSpan() << std::endl;
+    std::cout << "Longest span for sp3copy: " << sp3_copy.longestSpan() << std::endl;
 
     return (0);
 }
