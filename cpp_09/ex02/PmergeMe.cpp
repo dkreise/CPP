c9f2edb1201667d56c70c20b1790b6f9d6c451bd
Diana Kreise <dia.kreis@gmail.com>
2024-07-22 19:07:01 +0200

    alogirthm for deque

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -19,24 +19,35 @@ std::vector<int> PmergeMe::pmerge(char** nums)
 {
     // everything in try catch
 
-    std::vector<int> arr;
-    std::vector<int> sorted;
+    std::vector<int> arrV;
+    std::vector<int> sortedV;
+    std::deque<int> arrD;
+    std::deque<int> sortedD;
     
-    arr = parse(nums);
+    arrV = parseV(nums);
+    arrD = parseD(nums);
     std::cout << "Before: ";
-    printVector(arr);
+    printVector(arrV);
     
     clock_t startv = clock();
-    sorted = mergeInsertSortVector(arr);
+    sortedV = mergeInsertSortVector(arrV);
     clock_t endv = clock();
 
-    std::cout << "After: ";
-    printVector(sorted);
+    clock_t startd = clock();
+    sortedD = mergeInsertSortDeque(arrD);
+    clock_t endd = clock();
+
+    std::cout << "After vector: ";
+    printVector(sortedV);
+    std::cout << "After deque: ";
+    printDeque(sortedD);
 
     double elapsed = static_cast<double>(endv - startv) / CLOCKS_PER_SEC * 1000000.0;
-    std::cout << "Time to process a range of " << arr.size() << " elements with std::vector : " << elapsed << " us" << std::endl;
+    std::cout << "Time to process a range of " << arrV.size() << " elements with std::vector : " << elapsed << " us" << std::endl;
+    elapsed = static_cast<double>(endd - startd) / CLOCKS_PER_SEC * 1000000.0;
+    std::cout << "Time to process a range of " << arrD.size() << " elements with std::deque : " << elapsed << " us" << std::endl;
 
-    return (sorted);
+    return (sortedV);
 }
 
 std::vector<int> PmergeMe::mergeInsertSortVector(std::vector<int> arr)
@@ -44,7 +55,7 @@ std::vector<int> PmergeMe::mergeInsertSortVector(std::vector<int> arr)
     std::vector<int> sorted;
     int n;
     bool is_odd;
-    std::vector<std::pair<int, int>> pairs;
+    std::vector<std::pair<int, int> > pairs;
 
     n = arr.size();
     if (n % 2 == 1)    
@@ -62,7 +73,28 @@ std::vector<int> PmergeMe::mergeInsertSortVector(std::vector<int> arr)
     return (sorted);
 }
 
-std::vector<int> PmergeMe::parse(char** nums)
+std::deque<int> PmergeMe::mergeInsertSortDeque(std::deque<int> arr)
+{
+    std::deque<int> sorted;
+    int n;
+    bool is_odd;
+    std::deque<std::pair<int, int> > pairs;
+
+    n = arr.size();
+    if (n % 2 == 1)    
+        is_odd = true;
+    else 
+        is_odd = false;
+    pairs = getSortedPairsD(arr);
+    
+    sorted = getSortedDeque(pairs);
+    if (is_odd)
+        insertBSDeque(sorted, sorted.size(), arr[n - 1]);
+
+    return (sorted);
+}
+
+std::vector<int> PmergeMe::parseV(char** nums)
 {
     std::vector<int> arr;
     // parse properly later (check each string if is digigt, if is int etc)
@@ -73,10 +105,21 @@ std::vector<int> PmergeMe::parse(char** nums)
     return (arr);
 }
 
-std::vector<std::pair<int, int>> PmergeMe::getSortedPairsV(std::vector<int>& arr)
+std::deque<int> PmergeMe::parseD(char** nums)
+{
+    std::deque<int> arr;
+    // parse properly later (check each string if is digigt, if is int etc)
+    for (int i = 1; nums[i] != NULL; i ++)
+    {
+        arr.push_back(std::atoi(nums[i]));
+    }
+    return (arr);
+}
+
+std::vector<std::pair<int, int> > PmergeMe::getSortedPairsV(std::vector<int>& arr)
 {
     int n = arr.size();
-    std::vector<std::pair<int, int>> pairs;
+    std::vector<std::pair<int, int> > pairs;
 
     for (int i = 0; i < n; i += 2)
     {
@@ -92,23 +135,54 @@ std::vector<std::pair<int, int>> PmergeMe::getSortedPairsV(std::vector<int>& arr
     return (sortPairsV(pairs));
 }
 
-std::vector<std::pair<int, int>> PmergeMe::sortPairsV(std::vector<std::pair<int, int>> pairs)
+std::deque<std::pair<int, int> > PmergeMe::getSortedPairsD(std::deque<int>& arr)
+{
+    int n = arr.size();
+    std::deque<std::pair<int, int> > pairs;
+
+    for (int i = 0; i < n; i += 2)
+    {
+        if (i + 1 == n)
+            continue;
+        else if (arr[i] > arr[i + 1])
+            pairs.push_back(std::make_pair(arr[i], arr[i + 1]));
+        else
+            pairs.push_back(std::make_pair(arr[i + 1], arr[i]));
+    }
+    return (sortPairsD(pairs));
+}
+
+std::vector<std::pair<int, int>  > PmergeMe::sortPairsV(std::vector<std::pair<int, int> > pairs)
 {
     int n = pairs.size();
     if (n <= 1)
         return (pairs);
     
-    std::vector<std::pair<int, int>> left;
-    std::vector<std::pair<int, int>> right;
+    std::vector<std::pair<int, int> > left;
+    std::vector<std::pair<int, int> > right;
     std::copy(pairs.begin(), pairs.begin() + n / 2, std::back_inserter(left));
     std::copy(pairs.begin() + n / 2, pairs.end(), std::back_inserter(right));
 
     return (mergePairsV(sortPairsV(left), sortPairsV(right)));
 }
 
-std::vector<std::pair<int, int>> PmergeMe::mergePairsV(std::vector<std::pair<int, int>> left, std::vector<std::pair<int, int>> right)
+std::deque<std::pair<int, int> > PmergeMe::sortPairsD(std::deque<std::pair<int, int> > pairs)
 {
-    std::vector<std::pair<int, int>> sorted_pairs;
+    int n = pairs.size();
+    if (n <= 1)
+        return (pairs);
+    
+    std::deque<std::pair<int, int> > left;
+    std::deque<std::pair<int, int> > right;
+    std::copy(pairs.begin(), pairs.begin() + n / 2, std::back_inserter(left));
+    std::copy(pairs.begin() + n / 2, pairs.end(), std::back_inserter(right));
+
+    return (mergePairsD(sortPairsD(left), sortPairsD(right)));
+}
+
+std::vector<std::pair<int, int> > PmergeMe::mergePairsV(std::vector<std::pair<int, int> > left, std::vector<std::pair<int, int> > right)
+{
+    std::vector<std::pair<int, int> > sorted_pairs;
     int n1 = left.size();
     int n2 = right.size();
 
@@ -131,7 +205,32 @@ std::vector<std::pair<int, int>> PmergeMe::mergePairsV(std::vector<std::pair<int
     return (sorted_pairs);
 }
 
-std::vector<int> PmergeMe::getSortedVector(std::vector<std::pair<int, int>> pairs)
+std::deque<std::pair<int, int> > PmergeMe::mergePairsD(std::deque<std::pair<int, int> > left, std::deque<std::pair<int, int> > right)
+{
+    std::deque<std::pair<int, int> > sorted_pairs;
+    int n1 = left.size();
+    int n2 = right.size();
+
+    int i1 = 0;
+    int i2 = 0;
+    while (i1 < n1 || i2 < n2)
+    {
+        if (i1 != n1 && (i2 == n2 || (left[i1].first < right[i2].first)))
+        {
+            sorted_pairs.push_back(left[i1]);
+            i1 ++;
+        }
+        else
+        {
+            sorted_pairs.push_back(right[i2]);
+            i2 ++;
+        }
+    }
+
+    return (sorted_pairs);
+}
+
+std::vector<int> PmergeMe::getSortedVector(std::vector<std::pair<int, int> > pairs)
 {
     std::vector<int> sorted;
     int n = pairs.size();
@@ -158,6 +257,41 @@ std::vector<int> PmergeMe::getSortedVector(std::vector<std::pair<int, int>> pair
         {
             if (i <= n)
                 insertBSVector(sorted, len, pairs[i - 1].second);
+            i ++;
+        }
+        i ++;
+    }
+
+    return (sorted);
+}
+
+std::deque<int> PmergeMe::getSortedDeque(std::deque<std::pair<int, int> > pairs)
+{
+    std::deque<int> sorted;
+    int n = pairs.size();
+
+    for (int i = 0; i < n; i ++)
+        sorted.push_back(pairs[i].first);
+    std::cout << "Deque now: ";
+    printDeque(sorted);
+
+    int j_prevprev = 0;
+    int j_prev = 1;
+    int j_cur;
+    int len = 0;
+    int p = 0;
+    int i = 1;
+
+    while (i <= n)
+    {
+        j_cur = nextJacobsthal(j_prev, j_prevprev);
+        len = nextLength(len, p);
+        if (j_cur <= n)
+            insertBSDeque(sorted, len, pairs[j_cur - 1].second);
+        while (i < j_cur)
+        {
+            if (i <= n)
+                insertBSDeque(sorted, len, pairs[i - 1].second);
             i ++;
         }
         i ++;
@@ -211,6 +345,36 @@ void PmergeMe::insertBSVector(std::vector<int>& sorted, int len, int val)
     sorted.insert(sorted.begin() + l, val);
 }
 
+void PmergeMe::insertBSDeque(std::deque<int>& sorted, int len, int val)
+{
+    if (sorted.size() == 0)
+    {
+        sorted.push_back(val);
+        return;
+    }
+
+    int l = 0;
+    int h = len - 1;
+    int mid;
+    int cur;
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+        cur = sorted[mid];
+        if (l == h)
+        {
+            if (cur < val)
+                l ++;
+            break;
+        }
+        else if (cur < val)
+            l = mid + 1;
+        else    
+            h = mid - 1;
+    }
+    sorted.insert(sorted.begin() + l, val);
+}
+
 void PmergeMe::printVector(std::vector<int> v)
 {
     int n = v.size();
@@ -225,7 +389,21 @@ void PmergeMe::printVector(std::vector<int> v)
     std::cout << " ]" << std::endl;
 }
 
-void PmergeMe::printPairsV(std::vector<std::pair<int, int>> p)
+void PmergeMe::printDeque(std::deque<int> d)
+{
+    int n = d.size();
+
+    std::cout << "[ ";
+    for (int i = 0; i < n; i ++)
+    {
+        std::cout << d[i];
+        if (i != n - 1)
+            std::cout << ", ";
+    }
+    std::cout << " ]" << std::endl;
+}
+
+void PmergeMe::printPairsV(std::vector<std::pair<int, int> > p)
 {
     int n = p.size();
 
--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <deque>
 #include <ctime>
+#include <cstdlib>
 
 class PmergeMe
 {
@@ -22,22 +23,23 @@ class PmergeMe
         // help functions:
         static std::vector<int> mergeInsertSortVector(std::vector<int> arr);
         static std::deque<int> mergeInsertSortDeque(std::deque<int> arr);
-        static std::vector<int> parse(char** nums);
+        static std::vector<int> parseV(char** nums);
+        static std::deque<int> parseD(char** nums); // change, just copy from vector
 
-        static std::vector<std::pair<int, int>> getSortedPairsV(std::vector<int>& arr);
-        static std::deque<std::pair<int, int>> getSortedPairsD(std::deque<int>& arr);
-        static std::vector<std::pair<int, int>> sortPairsV(std::vector<std::pair<int, int>> pairs);
-        static std::deque<std::pair<int, int>> sortPairsD(std::deque<std::pair<int, int>> pairs);
-        static std::vector<std::pair<int, int>> mergePairsV(std::vector<std::pair<int, int>> left, std::vector<std::pair<int, int>> right);
-        static std::deque<std::pair<int, int>> mergePairsD(std::deque<std::pair<int, int>> left, std::deque<std::pair<int, int>> right);
+        static std::vector<std::pair<int, int> > getSortedPairsV(std::vector<int>& arr);
+        static std::deque<std::pair<int, int> > getSortedPairsD(std::deque<int>& arr);
+        static std::vector<std::pair<int, int> > sortPairsV(std::vector<std::pair<int, int> > pairs);
+        static std::deque<std::pair<int, int> > sortPairsD(std::deque<std::pair<int, int> > pairs);
+        static std::vector<std::pair<int, int> > mergePairsV(std::vector<std::pair<int, int> > left, std::vector<std::pair<int, int> > right);
+        static std::deque<std::pair<int, int> > mergePairsD(std::deque<std::pair<int, int> > left, std::deque<std::pair<int, int> > right);
 
         static void printVector(std::vector<int> v);
         static void printDeque(std::deque<int> d);
-        static void printPairsV(std::vector<std::pair<int, int>> p);
-        static void printPairsD(std::deque<std::pair<int, int>> p);
+        static void printPairsV(std::vector<std::pair<int, int> > p);
+        //static void printPairsD(std::deque<std::pair<int, int> > p);
 
-        static std::vector<int> getSortedVector(std::vector<std::pair<int, int>> pairs);
-        static std::deque<int> getSortedDeque(std::deque<std::pair<int, int>> pairs);
+        static std::vector<int> getSortedVector(std::vector<std::pair<int, int> > pairs);
+        static std::deque<int> getSortedDeque(std::deque<std::pair<int, int> > pairs);
         static int nextJacobsthal(int& prev, int& prevprev);
         static int nextLength(int len, int& pow);
         static void insertBSVector(std::vector<int>& sorted, int len, int val);
